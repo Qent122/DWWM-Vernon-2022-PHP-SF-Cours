c2009c2aa038c439f72de48a7eb28153ac39bd1f
Frédéric NOËL <contact@fredericnoel.com>
2022-08-22 10:38:31 +0200

    Re commit initial ^^

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PublicController extends AbstractController
 {
@@ -27,8 +29,15 @@ class PublicController extends AbstractController
     #[Route('/contact', name: 'contact')]
     public function contact(): Response
     {
-        return $this->render('public/whoami.html.twig', [
+        $formulaire = $this->createFormBuilder()
+        ->add('Nom', TextType::class,
+            ['attr' => ['class' => 'formCss']])
+        ->add('Envoyer', SubmitType::class)
+        ->getForm();
+
+        return $this->render('public/contact.html.twig', [
             'controller_name' => 'Who am I?',
+            'frmContact' => $formulaire->createView()
         ]);
     }
 }
